09e622e743862f1984be99796f09b9e8af99bf85
Daniel Stonier <d.stonier@gmail.com>
2014-06-09 11:21:06 +0900

    Rebase from 'upstream'

--- gradle_plugins/src/main/groovy/org/ros/gradle_plugins/RosAndroid.groovy
+++ gradle_plugins/src/main/groovy/org/ros/gradle_plugins/RosAndroid.groovy
@@ -17,7 +17,7 @@ class RosAndroidPlugin implements Plugin<Project> {
             project.apply(plugin: 'ros')
         }
         project.extensions.create("rosandroid", RosAndroidPluginExtension)
-        project.rosandroid.buildToolsVersion = "19.0.3"
+        project.rosandroid.buildToolsVersion = "19.1.0"
         /*********************************************************************
          * Find the android plugin
          *********************************************************************/ 
@@ -26,7 +26,7 @@ class RosAndroidPlugin implements Plugin<Project> {
                 mavenCentral()
             }
             dependencies {
-                classpath 'com.android.tools.build:gradle:0.9.+'
+                classpath 'com.android.tools.build:gradle:0.11.+'
             }
         }
         /********************************************************************** 
